5c56f6b17053f25fec2bf451cd61f26abc5dda26
Johnathan238 <jonathanumana86@gmail.com>
2022-07-23 22:23:37 -0400

    learing rhe concept about Type inference

---
+++ basics.ts
@@ -0,0 +1,60 @@
+// ^ Primirives: number, string, boolean
+// ^ More complex types: arrays , objects
+// ^ Function types, parameters
+
+// ? Primitives
+
+// ! Primitive typeof : number only !
+// ! Lowercase only !
+let age: number 
+
+age = 12
+
+// ! Primitive typeof : string only !
+// ! Lowercase only !
+let userName: string
+
+userName = "Freshyy"
+
+
+// ! Primitive typeof : boolean only !
+// ! Lowercase only !
+let isStudent: boolean
+
+isStudent = false
+
+
+// ? More complex types
+
+// ! complex types typeof : array only !
+// ! Lowercase only !
+let hobbies: string[]
+
+hobbies = ['Football', 'Cooking', 'Cars']
+
+// ! complex types typeof : object only !
+// ! Lowercase only !
+let person: {
+  name: string
+  age: number
+}
+
+person = {
+  name: "Freshyy",
+  age: 1600
+}
+
+// ! complex types typeof : array of object !
+// ! Lowercase only !
+let people: {
+  name: string
+  age: number
+}[]
+
+// ? Type inference
+let course = "Angular - The Complete Guide"
+
+course = 19045
+
+
+
